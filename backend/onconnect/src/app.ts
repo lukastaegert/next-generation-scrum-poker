d773061a3e901639111b4403a0bf3a32b8d34dc3
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-04-09 15:09:02 +0200

    Start working on a local docker setup
    Signed-off-by: Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>

--- backend/onconnect/src/app.ts
+++ backend/onconnect/src/app.ts
@@ -4,12 +4,53 @@ import { APIGatewayProxyHandler } from 'aws-lambda/trigger/api-gateway-proxy';
 const ddb = new AWS.DynamoDB.DocumentClient({
   apiVersion: '2012-08-10',
   region: process.env.AWS_REGION,
+  endpoint: 'http://dynamodb:8000',
 });
 
 const EXPIRY_TIME_IN_HOUR = process.env.EXPIRY_TIME_IN_HOUR || '1';
 const TABLE_NAME = process.env.TABLE_NAME || 'scrum_poker';
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+// TODO export name needs to be updated in deployment as well, but only lambdaHandler seems to work out of the box
+export const lambdaHandler: APIGatewayProxyHandler = async (event) => {
+  process.stdout.write('=> Event\n');
+  process.stdout.write(JSON.stringify(event));
+  process.stdout.write('\n<= Event\n');
+
+  // TODO Just create the table on each run, we can fix this later once it works
+  const db = new AWS.DynamoDB({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION,
+    endpoint: 'http://dynamodb:8000',
+  });
+
+  process.stdout.write('=> Create Table\n');
+  await new Promise((resolve, reject) => {
+    db.createTable(
+      {
+        TableName: TABLE_NAME,
+        AttributeDefinitions: [{ AttributeName: 'primaryKey', AttributeType: 'S' }],
+        KeySchema: [{ AttributeName: 'primaryKey', KeyType: 'HASH' }],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5,
+        },
+        SSESpecification: {
+          Enabled: true,
+        },
+      },
+      function (err, data) {
+        if (err) {
+          console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+          reject(err);
+        } else {
+          console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+          resolve(data);
+        }
+      }
+    );
+  });
+  process.stdout.write('<= Created Table\n');
+
   const expiryDate = new Date(Date.now());
   expiryDate.setHours(expiryDate.getHours() + parseFloat(EXPIRY_TIME_IN_HOUR));
   const putParams = {
